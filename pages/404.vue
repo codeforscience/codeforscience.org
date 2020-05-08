9d4afb98e7187fe3fef9c055c9cf373273bb7d29
Joe Hand <joe@hand.email>
2020-05-08 12:45:48 -0700

    try adding 404 again...

---
+++ pages/404.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="lh-copy mw8">
+    <div class="pa3 ph5-ns">
+      <h2 class="f2 ttu fw5">
+        Page not found
+      </h2>
+      <article ref="pageContent" class="lh-copy measure-wide">
+        <nuxt-link to="/" tag="a">
+          Home
+        </nuxt-link>
+      </article>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: '404Page'
+}
+</script>
+
+<style>
+</style>
