b8036d740f1a62bf10657868bdca454772849390
Joe Hand <joe@hand.email>
2020-05-22 08:20:17 -0700

    oops

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,8 +47,8 @@ export default async () => {
     buildModules: [
       // Doc: https://github.com/nuxt-community/eslint-module
       '@nuxtjs/eslint-module',
-      'nuxt-purgecss'
-      '@aceforth/nuxt-optimized-images',
+      'nuxt-purgecss',
+      '@aceforth/nuxt-optimized-images'
     ],
     /*
     ** Nuxt.js modules
