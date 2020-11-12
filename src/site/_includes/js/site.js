98cc4ae1c6b325f730672db66e5bcf63e9f435a8
Joe Hand <joe@hand.email>
2020-11-11 19:28:25 -0800

    a11y improvements

--- src/site/_includes/js/site.js
+++ src/site/_includes/js/site.js
@@ -1,6 +1,10 @@
 btnHandler('.menu-btn', function(event){
   document.getElementById('menu').classList.toggle('open')
-  // event.target.classList.toggle('open');
+  event.target.classList.toggle('open');
   event.target.setAttribute('aria-expanded', 'true')
-  console.log('👋 Oh, hello there!');
+  if (document.querySelectorAll('.open').length) {
+    setTimeout(function () {
+      document.querySelectorAll('#menu a')[0].focus()
+    },500) // slower than visibily hidden css transition
+  }
 });
