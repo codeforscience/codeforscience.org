59a8d53fbcf0d9805894b965c2e40498843704e2
Joe Hand <joe@hand.email>
2020-11-18 20:50:26 -0800

    use airtable for projects data

--- src/site/sponsored-projects.11ty.js
+++ src/site/sponsored-projects.11ty.js
@@ -14,14 +14,14 @@ class People {
     const projectsPage = await api.pages.read({
       slug: 'projects'
     })
-    const projects = await api.pages.browse({
-      filter: 'tag:hash-fsp'
-    })
+    // const projects = await api.pages.browse({
+    //   filter: 'tag:hash-fsp'
+    // })
     return {
       title: 'Projects',
       layout: 'layouts/base.njk',
       templateEngineOverride: "11ty.js",
-      projects,
+      // projects,
       projectsPage
     }
   }
@@ -38,20 +38,48 @@ class People {
               ${projectsPage.html}
             </div>
             ${await Promise.all(projects.map(async (project) => {
+              if (project['Tags (css.org)'] && project['Tags (css.org)'].includes('alumni') || !project['Description (css.org)']) return
               return `<article
                 class="pv4 bt b--black-10 ph3 ph0-l mw7"
               >
                 <div class="black-70">
                   <div class="flex flex-column flex-row-ns items-center">
                     <div class="order-2 order-1-ns mb4 mb0-ns w-100 w-40-ns">
-                      ${await this.Image(project.feature_image, project.title, "db mw5-l mw4 center")}
+                      ${project["Logo"] && await this.Image(project["Logo"][0].url, project['Display Name'], "db mw5-l mw4 center")}
                     </div>
                     <div class="w-100 w-60-ns pl4-l pr3-ns order-1 order-2-ns">
                       <h1 class="f4 mt0 lh-title">
-                        ${ project.title }
+                        ${ project['Display Name'] }
                       </h1>
                       <div class="f5 f4-l lh-copy">
-                      ${project.html}
+                      ${project['Description (css.org)']}
+                      <p>Website <a href="${project['Website']}">${project['Website'].replace(/^https?:\/\//,'')}</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>`
+            })).then((arr) => arr.join(''))}
+            <h2 class="f2 ttu fw5 pt5">
+              Alumni
+            </h2>
+            ${await Promise.all(projects.map(async (project) => {
+              if (!project['Tags (css.org)'] || !project['Tags (css.org)'].includes('alumni')) return
+              return `<article
+                class="pv4 bt b--black-10 ph3 ph0-l mw7"
+              >
+                <div class="black-70">
+                  <div class="flex flex-column flex-row-ns items-center">
+                    <div class="order-2 order-1-ns mb4 mb0-ns w-100 w-40-ns">
+                      ${project["Logo"] && await this.Image(project["Logo"][0].url, project['Display Name'], "db mw5-l mw4 center")}
+                    </div>
+                    <div class="w-100 w-60-ns pl4-l pr3-ns order-1 order-2-ns">
+                      <h1 class="f4 mt0 lh-title">
+                        ${ project['Display Name'] }
+                      </h1>
+                      <div class="f5 f4-l lh-copy">
+                      ${project['Description (css.org)']}
+                      <p>Website <a href="${project['Website']}">${project['Website'].replace(/^https?:\/\//,'')}</a>
                       </div>
                     </div>
                   </div>
